6305eb343a4e4b3449c8bd90e83a59b689fc9d50
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-26 11:28:44 +0100

    Added `AbstractContext`

---
+++ src/AbstractContext.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dhii\Espresso;
+
+use Dhii\Data\ValueAwareInterface;
+
+/**
+ * Abstract implementation of a context.
+ *
+ * @since [*next-version*]
+ */
+abstract class AbstractContext implements ContextInterface, ValueAwareInterface
+{
+}
