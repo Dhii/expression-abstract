512ce084d3fd3e94b4febb2c84d04174cd4d0a32
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-26 13:40:42 +0100

    Added `EvalException`

---
+++ src/EvalException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dhii\Espresso;
+
+use \Exception;
+
+/**
+ * An exception that is thrown as a consequence of a problem during evaluation.
+ *
+ * @since [*next-version*]
+ */
+class EvalException extends Exception implements EvalExceptionInterface
+{
+}
