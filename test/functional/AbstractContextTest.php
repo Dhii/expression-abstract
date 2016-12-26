67371f24186b999ec615ec136373cab8381f9432
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-26 11:28:56 +0100

    Added functional test for `AbstractContext`

---
+++ test/functional/AbstractContextTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Dhii\Espresso\Test;
+
+use \Xpmock\TestCase;
+
+/**
+ * Tests {@see Dhii\Espresso\AbstractContext}.
+ *
+ * @since [*next-version*]
+ */
+class AbstractContextTest extends TestCase
+{
+    /**
+     * The name of the test subject.
+     */
+    const TEST_SUBJECT_CLASSNAME = '\\Dhii\\Espresso\\AbstractContext';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next=version*]
+     *
+     * @return Dhii\Espresso\AbstractContext
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->getValue()
+            ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @covers \Dhii\Espresso\AbstractContext
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject);
+        $this->assertInstanceOf('\\Dhii\\Espresso\\ContextInterface', $subject);
+        $this->assertInstanceOf('\\Dhii\\Data\\ValueAwareInterface', $subject);
+    }
+}
