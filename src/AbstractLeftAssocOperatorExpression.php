f286fac950154597f84b6949d57f2bd24530a113
Miguel Muscat <miguelmuscat93@gmail.com>
2017-01-15 11:52:38 +0100

    Added `AbstractLeftAssocOperatorExpression`

---
+++ src/AbstractLeftAssocOperatorExpression.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dhii\Espresso;
+
+/**
+ * Abstract implementation of a left associative expression.
+ *
+ * A left associative expression is an expression that evaluates its terms in the order they are given.
+ * ie. first to last, left to right.
+ *
+ * @since [*next-version*]
+ */
+abstract class AbstractLeftAssocOperatorExpression extends AbstractOperatorExpression
+{
+}
