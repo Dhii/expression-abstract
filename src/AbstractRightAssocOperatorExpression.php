db2efbcb7119aff98f69f5688b2928d7e655c3b6
Miguel Muscat <miguelmuscat93@gmail.com>
2017-01-15 11:53:09 +0100

    Added `AbstractRightAssocOperatorExpression`

---
+++ src/AbstractRightAssocOperatorExpression.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Dhii\Espresso;
+
+use Dhii\Data\ValueAwareInterface;
+
+/**
+ * Abstract implementation of a right associative expression.
+ *
+ * A right associative expression is an expression that evaluates its terms in reverse order.
+ * ie. last to first, right to left.
+ *
+ * @since [*next-version*]
+ */
+abstract class AbstractRightAssocOperatorExpression extends AbstractOperatorExpression
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    protected function _getOrderedTerms(ValueAwareInterface $ctx = null)
+    {
+        return array_reverse($this->getTerms());
+    }
+}
---
+++ test/unit/AbstractRightAssociativeExpressionTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Dhii\Espresso\Test;
+
+use \Dhii\Espresso\AbstractRightAssocOperatorExpression;
+use \Dhii\Evaluable\EvaluableInterface;
+use \Xpmock\TestCase;
+
+/**
+ * Tests {@see \Dhii\Espresso\AbstractRightAssocOperatorExpression}.
+ *
+ * @since [*next-version*]
+ */
+class AbstractRightAssocOperatorExpressionTest extends TestCase
+{
+
+    /**
+     * The name of the test subject.
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\\Espresso\\AbstractRightAssocOperatorExpression';
+
+    /**
+     * Creates an instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return AbstractRightAssocOperatorExpression
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->_initBuffer(function() {
+                return null;
+            })
+            ->_defaultValue(function() {
+                return 0;
+            })
+            ->_operator(function($left, $right) {
+                return is_null($left)
+                    ? $right
+                    : $left / $right;
+            })
+        ;
+
+        return $mock->new();
+    }
+
+    /**
+     * Creates a mock term instance that simply returns a specific value.
+     *
+     * @since [*next-version*]
+     *
+     * @param mixed $value The value to return.
+     *
+     * @return EvaluableInterface
+     */
+    public function mockTerm($value)
+    {
+        $mock = $this->mock('Dhii\\Evaluable\\EvaluableInterface')
+            ->evaluate($value);
+
+        return $mock->new();
+    }
+
+    /**
+     * Tests the evaluation without any terms.
+     *
+     * @since [*next-version*]
+     */
+    public function testNoTerms()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertEquals(0, $subject->evaluate());
+    }
+
+    /**
+     * Tests the evaluation with just 1 term.
+     *
+     * @since [*next-version*]
+     */
+    public function testSingleTerm()
+    {
+        $subject = $this->createInstance();
+        $subject->this()->terms = array(
+            $this->mockTerm(2)
+        );
+
+        $this->assertEquals(2, $subject->evaluate());
+    }
+
+    /**
+     * Tests the evaluation with 2 terms.
+     *
+     * @since [*next-version*]
+     */
+    public function testTwoTerms()
+    {
+        $subject = $this->createInstance();
+        $subject->this()->terms = array(
+            $this->mockTerm(2),
+            $this->mockTerm(10)
+        );
+
+        $this->assertEquals(5, $subject->evaluate());
+    }
+
+    /**
+     * Tests the evaluation with multiple terms.
+     *
+     * @since [*next-version*]
+     */
+    public function testMultipleTerms()
+    {
+        $subject = $this->createInstance();
+        $subject->this()->terms = array(
+            $this->mockTerm(2),
+            $this->mockTerm(10),
+            $this->mockTerm(4)
+        );
+
+        $this->assertEquals(0.2, $subject->evaluate());
+    }
+
+}
