2c8ceaec3557c3cbad7c7bd70522c68dab3f4a36
Miguel Muscat <miguelmuscat93@gmail.com>
2017-01-26 15:45:45 +0100

    Refactored `AbstractContext`
    - No longer implements `ContextInterface`
    - Has property and protected getter and setter methods

--- src/AbstractContext.php
+++ src/AbstractContext.php
@@ -2,13 +2,47 @@
 
 namespace Dhii\Expression;
 
-use Dhii\Data\ValueAwareInterface;
-
 /**
  * Abstract implementation of a context.
  *
  * @since [*next-version*]
  */
-abstract class AbstractContext implements ContextInterface, ValueAwareInterface
+abstract class AbstractContext
 {
+    /**
+     * The contextual value.
+     *
+     * @since [*next-version*]
+     *
+     * @var mixed
+     */
+    protected $value;
+
+    /**
+     * Retrieves the contextual value.
+     *
+     * @since [*next-version*]
+     *
+     * @return mixed The contextual value.
+     */
+    protected function _getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Sets the contextual value.
+     *
+     * @since [*next-version*]
+     *
+     * @param mixed $value The contextual value.
+     *
+     * @return $this This instance.
+     */
+    protected function _setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
 }
